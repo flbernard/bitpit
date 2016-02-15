bc3e331e731954a8f9b887b697ab68eb4a57b333
Alessandro Alaia <alessandro.alaia@optimad.it>
2016-02-15 17:26:26 +0100

    patch: Added member function Patch::findFaceNeighCell()

--- src/patch/patch.hpp
+++ src/patch/patch.hpp
@@ -147,6 +147,7 @@ public:
 	std::vector<long> findCellVertexNeighs(const long &id, bool complete = true) const;
 	std::vector<long> findCellVertexNeighs(const long &id, const int &vertex, const std::vector<long> &blackList = std::vector<long>()) const;
 	std::vector<long> findCellVertexNeighs(const long &id, const std::vector<int> &vertices, const std::vector<long> &blackList = std::vector<long>()) const;
+        void findFaceNeighCell(const long &cell_idx, const long &neigh_idx, int &face_loc_idx, int &intf_loc_idx);
 
 	CellIterator getCellIterator(const long &id);
 	CellIterator cellBegin();
